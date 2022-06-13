7e215e14c7d5f32e783cb3fca29b55dbeb5b9da3
josia <87546094+Josyas@users.noreply.github.com>
2022-06-13 20:04:22 -0300

    ex lista

---
+++ listaTeste/Entites/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace listaTeste.Entites
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public double Grades { get; set; }
+
+        public Student(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id
+                 + "\nName: " + Name
+                 + "\nLast name: " + LastName;
+        }
+    }
+}
---
+++ listaTeste/Program.cs
@@ -0,0 +1,68 @@
+﻿using listaTeste.Entites;
+using System;
+using System.Collections.Generic;
+
+namespace listaTeste
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> list = new();
+
+            Console.WriteLine("Do you want to register the student? ");
+            int register = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < register; i++)
+            {
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Last name: ");
+                string lastName = Console.ReadLine();
+                list.Add(new Student(id, name, lastName));
+            }
+
+            foreach(var student in list)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("Edit student registration");
+            
+            Console.WriteLine();
+            Console.WriteLine("Name: ");
+            string nameEdit = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Last name: ");
+            string lastNameEdit = Console.ReadLine();
+
+            Student studentEdit = list.Find(s => s.Name == nameEdit);
+            if(studentEdit != null)
+            {
+                Console.Write("New name: ");
+                string name = Console.ReadLine();
+                studentEdit.Name = name;
+            }
+            else
+            {
+                Console.WriteLine("this name not exist!");
+            }
+
+            studentEdit = list.Find(l => l.LastName == lastNameEdit);
+            if(studentEdit == null)
+            {
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of student!");
+            foreach(var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
